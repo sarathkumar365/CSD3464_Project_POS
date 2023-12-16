2313d66eba4330a4d6b602d00db8224b2ab96873
rositta <annroy2708@gmail.com>
2023-12-15 19:33:57 -0500

    controller class

--- src/Controller.java
+++ src/Controller.java
@@ -34,13 +34,13 @@ public class Controller {
         int choice;
         do {
             // Display the main menu to the user.
-            System.out.println("------Adding Fuel to inventory  -------");
+            System.out.println("------Adding Fuel to inventory in litres  -------");
             System.out.println("1. Regular Petrol");
             System.out.println("2. Regular Diesel");
             System.out.println("3. vpower Petrol");
             System.out.println("4. vpower Diesel");
             System.out.println("5. Exit");
-             choice = Integer.parseInt(scanner.next());
+            choice = Integer.parseInt(scanner.next());
             switch (choice) {
                 case 1: {
                     System.out.println("Please enter the petrol to be added to Regular Petrol Fuel Inventory ");
@@ -81,50 +81,50 @@ public class Controller {
 
 
     }
-public String selectGrade(String type){
+    public String selectGrade(String type){
 
-    int fuelGradeUserChoice;
-    String fuelGradeChoice="NAN";
-    // Display the main menu to the user.
-    System.out.println("------Select the Grade-------");
-    System.out.println("1. Regular");
-    System.out.println("2. Vpower");
-    System.out.println("3. Exit");
-    fuelGradeUserChoice =Integer.parseInt(scanner.next());
-    switch (fuelGradeUserChoice) {
-        case 1:
+        int fuelGradeUserChoice;
+        String fuelGradeChoice="NAN";
+        // Display the main menu to the user.
+        System.out.println("------Select the Grade-------");
+        System.out.println("1. Regular");
+        System.out.println("2. Vpower");
+        System.out.println("3. Exit");
+        fuelGradeUserChoice =Integer.parseInt(scanner.next());
+        switch (fuelGradeUserChoice) {
+            case 1:
 
-            if (type.equalsIgnoreCase("petrol")) {
-                fuelGradeChoice = "regular";
-                fuelType = new Petrol(Constants.petrolRegularPrice, FuelType.Grade.regular);
-            }
-            else if (type.equalsIgnoreCase("diesel")){
-                fuelGradeChoice = "regular";
+                if (type.equalsIgnoreCase("petrol")) {
+                    fuelGradeChoice = "regular";
+                    fuelType = new Petrol(Constants.petrolRegularPrice, FuelType.Grade.regular);
+                }
+                else if (type.equalsIgnoreCase("diesel")){
+                    fuelGradeChoice = "regular";
 
-                fuelType = new Diesel(Constants.dieselRegularPrice, FuelType.Grade.regular);
-            }
-            break;
-        case 2:
+                    fuelType = new Diesel(Constants.dieselRegularPrice, FuelType.Grade.regular);
+                }
+                break;
+            case 2:
 
-            if (type.equalsIgnoreCase("diesel")) {
-                fuelGradeChoice = "vpower";
-                fuelType = new Petrol(Constants.petrolvpowerPrice, FuelType.Grade.vpower);
-            }
-            else{
-            fuelGradeChoice = "vpower";
-            fuelType = new Diesel(Constants.dieselvpowerPrice, FuelType.Grade.vpower);
+                if (type.equalsIgnoreCase("diesel")) {
+                    fuelGradeChoice = "vpower";
+                    fuelType = new Petrol(Constants.petrolvpowerPrice, FuelType.Grade.vpower);
+                }
+                else{
+                    fuelGradeChoice = "vpower";
+                    fuelType = new Diesel(Constants.dieselvpowerPrice, FuelType.Grade.vpower);
+                }
+                break;
+
+            case 3 :
+                System.out.println("Exiting the Fuel Approval option");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+
         }
-            break;
-
-        case 3 :
-            System.out.println("Exiting the Fuel Approval option");
-            break;
-        default:
-            System.out.println("Invalid choice. Please try again.");
-
+        return fuelGradeChoice.toLowerCase();
     }
-return fuelGradeChoice.toLowerCase();
-}
 
     public void approveFuel(String fuelGrade) {
         int pumpno;
@@ -142,32 +142,37 @@ return fuelGradeChoice.toLowerCase();
                 throw new NoSuchElementException("Invalid pump number");
             }
 
-            if (!fuelDispenser.get(pumpno).isPumpOccupied()) {
+            if (!fuelDispenser.get(pumpno-1).isPumpOccupied()) {
                 System.out.println("Enter the amount(currency) to fill up ");
                 double rate = scanner.nextDouble();
                 System.out.println("Enter the amount(currency) paid by user ");
                 double paidAmount = scanner.nextDouble();
-                double approvedFuel = fuelDispenser.get(pumpno).approveFuel(rate, paidAmount, fuelType);
-                System.out.println("Enter the amount of fuel used in litres");
-                double useFuel = scanner.nextDouble();
-                fuelDispenser.get(pumpno).dispenseFuel(approvedFuel, useFuel);
+                if(paidAmount>=rate) {
+                    double approvedFuel = fuelDispenser.get(pumpno - 1).approveFuel(rate, paidAmount, fuelType);
+                    System.out.println("Enter the amount of fuel used in litres");
+                    double useFuel = scanner.nextDouble();
+                    fuelDispenser.get(pumpno - 1).dispenseFuel(approvedFuel, useFuel);
 
-                switch (fuelGrade) {
-                    case "regularpetrol":
-                        regularpetrolInventory.updateDispensedQuantity(useFuel);
-                        break;
-                    case "vpowerpetrol":
-                        vpowerpetrolInventory.updateDispensedQuantity(useFuel);
-                        break;
-                    case "regulardiesel":
-                        regulardieselInventory.updateDispensedQuantity(useFuel);
-                        break;
-                    case "vpowerdiesel":
-                        vpowerdieselInventory.updateDispensedQuantity(useFuel);
-                        break;
+                    switch (fuelGrade) {
+                        case "regularpetrol":
+                            regularpetrolInventory.updateDispensedQuantity(useFuel);
+                            break;
+                        case "vpowerpetrol":
+                            vpowerpetrolInventory.updateDispensedQuantity(useFuel);
+                            break;
+                        case "regulardiesel":
+                            regulardieselInventory.updateDispensedQuantity(useFuel);
+                            break;
+                        case "vpowerdiesel":
+                            vpowerdieselInventory.updateDispensedQuantity(useFuel);
+                            break;
+                    }
+
+                    TranscationLogger.dologFuelTransaction(approvedFuel, useFuel);
                 }
-
-                TranscationLogger.dologFuelTransaction(approvedFuel, useFuel);
+                else {
+                    System.out.println("Amount insufficient ");
+                }
             }
         } catch (InputMismatchException e) {
             System.out.println("Invalid input. Please enter a valid number.");
@@ -183,46 +188,56 @@ return fuelGradeChoice.toLowerCase();
 
     public ArrayList<Integer> showPumpStatus(){
         ArrayList<Integer> pumpNumber=new ArrayList<>();
-        for(int i=0;i<fuelDispenser.size();i++){
-            if(!fuelDispenser.get(i).isPumpOccupied())
+        for(int i=1;i<=fuelDispenser.size();i++){
+            if(!fuelDispenser.get(i-1).isPumpOccupied())
             {
                 pumpNumber.add(i) ;
             }
         }
 
-  return pumpNumber;
+        return pumpNumber;
     }
     public void refundMoney() {
         ArrayList<Integer> pumpNumber = new ArrayList<>();
-        for (int i = 0; i < fuelDispenser.size(); i++) {
-            if (fuelDispenser.get(i).isPumpOccupied()) {
+        for (int i = 1; i <= fuelDispenser.size(); i++) {
+            if (fuelDispenser.get(i - 1).isPumpOccupied()) {
                 pumpNumber.add(i);
                 System.out.println("Pump " + i);
             }
         }
-
-        System.out.println("------Select the pump for Refund-------");
         try {
-            int pumpno = Integer.parseInt(scanner.next());
+            if (pumpNumber.size() > 0) {
+                System.out.println("------Select the pump for Refund-------");
 
-            if (pumpno < 0 ) {
-                throw new NoSuchElementException("Invalid pump number");
+                int pumpno = Integer.parseInt(scanner.next());
+
+                if (pumpno < 0) {
+                    throw new NoSuchElementException("Invalid pump number");
+                }
+
+                double refundMoney = fuelDispenser.get(pumpno - 1).refundAmount(fuelType);
+                System.out.println("Refund Issued (currency) " + refundMoney);
             }
 
-            double refundMoney = fuelDispenser.get(pumpno).refundAmount(fuelType);
-            System.out.println("Refund Issued (currency) " + refundMoney);
-        } catch (NumberFormatException e) {
+            else {
+                System.out.println("All pumps are available.No refund needed");
+            }
+        } catch(NumberFormatException e){
             System.out.println("Invalid input. Please enter a valid number for pump selection.");
-        } catch (NoSuchElementException e) {
+        } catch(NoSuchElementException e){
             System.out.println("Invalid pump number. Please select a pump that is currently occupied.");
-        } catch (NullPointerException e) {
+        } catch(NullPointerException e){
             System.out.println("Fuel dispenser not available for the selected pump.");
-        } catch (Exception e) {
+        } catch(Exception e){
             System.out.println("An unexpected error occurred.");
         }
+
     }
 
-
-
-
 }
+
+
+
+
+
+
